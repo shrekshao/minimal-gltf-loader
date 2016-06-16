31e3b17ef057908a22781653e13448431b110690
shrekshao <shrekshao@gmail.com>
2016-06-16 11:51:16 -0400

    gulp jshint, jshint style tweaks, names

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
+'use strict';
+
+var jshint = require('gulp-jshint');
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var jsHintFiles = [
+        '**/*.js', 
+        '**/*.html', 
+        '!node_modules/**', 
+        '!src/third-party/**',
+        '!gulpfile.js'
+        ];
+
+// gulp.task('default', function() {
+//   // place code for your default task here
+// });
+
+gulp.task('default', ['jsHint']);
+
+gulp.task('jsHint', function() {
+    return gulp.src(jsHintFiles)
+        .pipe(jshint.extract('auto'))
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('jsHint-watch', function() {
+    gulp.watch(jsHintFiles).on('change', function(event) {
+        gulp.src(event.path)
+            .pipe(jshint.extract('auto'))
+            .pipe(jshint())
+            .pipe(jshint.reporter('jshint-stylish'));
+    });
 });
